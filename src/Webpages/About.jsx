ad47e210d8d1655b5fd59e43b616f15e90ec9fce
Shania Chacon <chacon1shania@gmail.com>
2025-04-15 09:48:39 -0400

    Update About.jsx

--- src/Webpages/About.jsx
+++ src/Webpages/About.jsx
@@ -8,8 +8,8 @@ function About() {
       <Navbar />
       <HeadingDesc
         title="Product Designer"
-        paragraph="Currently pursuing her Bachelor of Fine Arts in Digital Media and Design, she specializes in crafting user-centric web and interactive media experiences that seamlessly blend simplicity with functionality. With an eye for detail and a strong belief that great design should be both elegant and intuitive, Shania’s work reflects a balance between aesthetics and usability. She draws inspiration from contemporary digital tools and timeless design principles, ensuring her creations are not only visually appealing but also highly effective."
-        paragraph2="hello"
+        paragraph="I'm currently working toward my Bachelor of Fine Arts in Digital Media and Design, focusing on creating user-centered web and interactive experiences that are both simple and functional. I’ve always believed that good design should feel effortless—clean, intuitive, and thoughtful. I love paying attention to the little details that make a big difference, and I’m constantly inspired by a mix of modern digital tools and classic design principles. My goal is to make things that not only look great but work great too."
+        paragraph2="I started my UX/UI journey in college while searching for a way to combine my love for design and coding. Since then, it’s been quite the ride—from interning and working at ArtsWestchester to taking on freelance projects. Each step has taught me something new and helped me grow not just as a designer, but as a creative problem-solver."
       />
     </div>
   );
