a3ab5c90ef1dcf7945f16bf0a92d4ff47bcf09cd
Shania Chacon <chacon1shania@gmail.com>
2025-04-15 09:53:11 -0400

    small padding changes

--- sections/PageLayout/HeadingDesc.jsx
+++ sections/PageLayout/HeadingDesc.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function HeadingDesc({ title, paragraph, paragraph2 }) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:grid-rows-1 gap-4 m-4 flex-wrap">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:grid-rows-1 gap-4 m-4 py-14 flex-wrap">
       <div className="lg:col-span-1">
         <h1 className="text-black rounded-lg text-4xl">{title}</h1>
       </div>
