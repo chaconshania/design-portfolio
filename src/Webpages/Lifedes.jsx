6ba6bc715b3907a660d995a7f5405413494b29ef
Shania Chacon <chacon1shania@gmail.com>
2025-04-18 01:12:58 -0400

    adding content

--- src/Webpages/Lifedes.jsx
+++ src/Webpages/Lifedes.jsx
@@ -12,13 +12,20 @@ function Lifedes() {
         productType="Design"
         productRole="UX/UI Designer"
         productDuration="1 Week"
-        description="Many people struggle to make the most of their wardrobe, often overlooking pieces they already own. With Fitmix, I set out to simplify this process by creating a mobile app prototype that lets users snap photos of their clothes and get fresh outfit inspiration—right from their closet."
-        description2="This design challenge was completed as part of my UI Design course, where I focused on building a high-fidelity, interactive prototype. The goal was to create a visually engaging and user-friendly experience that encourages personal style exploration and more intentional fashion choices."
+        description="Designing a modern, responsive fashion and lifestyle digital publication that provides an engaging, editorial-style reading experience for users on both web and mobile platforms."
         image="/assets/Images/Lifedes/Lifedes_cover.png"
       />
       <HeadingDesc
         title="Project Objectives"
-        paragraph="Snap photos of your clothes or find fresh outfit inspiration to make the most of every piece in your closet."
+        paragraph="This project aims to design a responsive fashion and lifestyle digital publication that reflects current trends through bold visuals, clean layouts, and editorial-style storytelling. The goal is to create an engaging, mobile-friendly experience that blends aesthetic appeal with intuitive navigation, accessible design, and flexible content modules—bringing the look and feel of a modern fashion magazine into the digital space."
+      />
+      <HeadingDesc
+        title="Problem Statement"
+        paragraph="Fashion and lifestyle readers, especially mobile-first users, struggle to find digital publications that balance bold, trend-driven visuals with clean, intuitive design. Current platforms often feel cluttered, outdated, and lack responsiveness—leading to a disjointed and unsatisfying user experience."
+      />
+      <HeadingDesc
+        title="The Solution"
+        paragraph="To solve this, I designed a responsive digital publication that blends sleek, modern visuals with clean, intuitive layouts. The goal was to create a seamless experience across all devices, ensuring users could enjoy stylish, engaging content without feeling overwhelmed. "
       />
     </div>
   );
